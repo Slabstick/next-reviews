57743e00f5e2a4dd235c4167c385b8dfc68ded42
Jakob Klimczok <jklimczok@veda.fritz.box>
2023-06-18 20:00:28 +0200

    first commit

---
+++ app/about/page.jsx
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+	return (
+		<>
+			<h1>About</h1>
+			<p>This will be the about page.</p>
+		</>
+
+	)
+}
---
+++ app/reviews/hollow-knight/page.jsx
@@ -0,0 +1,9 @@
+export default function HollowKnightPage() {
+	return (
+		<>
+			<h1>Hollow Knight</h1>
+			<p>This will be the review for Hollow Knight.</p>
+		</>
+
+	)
+}
